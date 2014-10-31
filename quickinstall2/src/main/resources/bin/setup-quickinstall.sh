42dacc563cdfd8203aa01039a11a62dcaed5dce0
Michael Wall <mjwall@gmail.com>
2014-10-31 09:22:09 -0400

    Fix env file
    Pull out sed stuff to a function, works differently between Mac and Linux

--- quickinstall2/src/main/resources/bin/setup-quickinstall.sh
+++ quickinstall2/src/main/resources/bin/setup-quickinstall.sh
@@ -2,6 +2,7 @@
 
 # this script is meant to be run once.  It could be bad to run this again
 
+# internal functions
 _script_dir() {
     if [ -z "${SCRIPT_DIR}" ]; then
     # even resolves symlinks, see
@@ -11,6 +12,17 @@ _script_dir() {
         SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
     fi
     echo "${SCRIPT_DIR}"
+}
+
+_replace_stuff() {
+  local TO=$1
+  local FROM=$2
+  local FILE=$3
+  if [ "$(uname)" == "Darwin" ]; then
+    sed -i '' -e "s|${TO}|${FROM}|g" ${FILE}
+  else
+    sed -i'' -e "s|${TO}|${FROM}|g" ${FILE}
+  fi
 }
 
 # just cut off /bin/
@@ -63,12 +75,12 @@ check_ssh() {
 setup_hadoop_conf() {
   echo "Setting up Hadoop conf"
   HADOOP_HOME="${QI_HOME}/hadoop-2.4.1"
-  sed -i'' -e "s|QI_HADOOP_HOME|${HADOOP_HOME}|" ${HADOOP_HOME}/etc/hadoop/hadoop-env.sh
-  sed -i'' -e "s|QI_JAVA_HOME|${JAVA_HOME}|" ${HADOOP_HOME}/etc/hadoop/hadoop-env.sh
+  _replace_stuff "QI_HADOOP_HOME" "${HADOOP_HOME}" ${HADOOP_HOME}/etc/hadoop/hadoop-env.sh
+  _replace_stuff "QI_JAVA_HOME" "${JAVA_HOME}" ${HADOOP_HOME}/etc/hadoop/hadoop-env.sh
   HDFS_DIR="${QI_HOME}/hdfs"
   mkdir -p ${HDFS_DIR}/name
   mkdir -p ${HDFS_DIR}/data
-  sed -i'' -e "s|QI_HDFS_DIR|${HDFS_DIR}|g" ${HADOOP_HOME}/etc/hadoop/hdfs-site.xml
+  _replace_stuff "QI_HDFS_DIR" "${HDFS_DIR}" ${HADOOP_HOME}/etc/hadoop/hdfs-site.xml
   export HADOOP_HOME
 }
 
@@ -95,10 +107,10 @@ start_hadoop() {
 setup_zookeeper_conf() {
   echo "Setting up Zookeeper conf"
   ZOOKEEPER_HOME="${QI_HOME}/zookeeper-3.4.6"
-  sed -i'' -e "s|QI_ZOOKEEPER_HOME|${ZOOKEEPER_HOME}|" ${ZOOKEEPER_HOME}/bin/zkEnv.sh
+  _replace_stuff "QI_ZOOKEEPER_HOME" "${ZOOKEEPER_HOME}" ${ZOOKEEPER_HOME}/bin/zkEnv.sh
   ZOO_DATA="${QI_HOME}/zk-data"
   mkdir -p ${ZOO_DATA}
-  sed -i'' -e "s|QI_ZOO_DATA|${ZOO_DATA}|" ${ZOOKEEPER_HOME}/conf/zoo.cfg
+  _replace_stuff "QI_ZOO_DATA" "${ZOO_DATA}" ${ZOOKEEPER_HOME}/conf/zoo.cfg
   export ZOOKEEPER_HOME
 }
 
@@ -111,8 +123,8 @@ setup_accumulo_conf() {
   echo "Setting up Accumulo conf"
   ACCUMULO_HOME="${QI_HOME}/accumulo-1.6.1"
   cp -R ${ACCUMULO_HOME}/conf/examples/2GB/standalone/* ${ACCUMULO_HOME}/conf/.
-  sed -i'' -e "s|JAVA_HOME=/path/to/java|JAVA_HOME=${JAVA_HOME}|" ${ACCUMULO_HOME}/conf/accumulo-env.sh
-  sed -i'' -e "s|ZOOKEEPER_HOME=/path/to/zookeeper|ZOOKEEPER_HOME=${ZOOKEEPER_HOME}|" ${ACCUMULO_HOME}/conf/accumulo-env.sh
+  _replace_stuff "JAVA_HOME=/path/to/java" "JAVA_HOME=${JAVA_HOME}" ${ACCUMULO_HOME}/conf/accumulo-env.sh
+  _replace_stuff "ZOOKEEPER_HOME=/path/to/zookeeper" "ZOOKEEPER_HOME=${ZOOKEEPER_HOME}" ${ACCUMULO_HOME}/conf/accumulo-env.sh
   cat <<'EOF' >> ${ACCUMULO_HOME}/conf/accumulo-env.sh
 if [ "$(uname)" == "Darwin" ]; then
   # https://issues.apache.org/jira/browse/HADOOP-7489
@@ -137,38 +149,19 @@ start_accumulo() {
 }
 
 finish() {
-  echo "Creating the cloud-env file"
-  cloudenv="#!/bin/bash
-_script_dir() {
-    if [ -z \"\${SCRIPT_DIR}\" ]; then
-    # even resolves symlinks, see
-    # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
-        local SOURCE=\"\${BASH_SOURCE[0]}\"
-        while [ -h \"\$SOURCE\" ] ; do SOURCE=\"\$(readlink \"$SOURCE\")\"; done
-        SCRIPT_DIR=\"\$( cd -P \"$( dirname \"$SOURCE\" )\" && pwd )\"
-    fi
-    echo \"\${SCRIPT_DIR}\"
-}
-
-export QI_HOME=${QI_HOME}
-if [ \"\$(_script_dir)\" != \"\${QI_HOME}/bin\" ]; then
-  echo \"Looks like you moved the quickinstall.  Sorry, that is not supported.\"
-  unset QI_HOME
-else
-  echo \"Setting up the Accumulo quickinstall in \${QI_HOME}\"
-  export JAVA_HOME=\"${JAVA_HOME}\"
-  export HADOOP_HOME=\"${HADOOP_HOME}\"
-  export ZOOKEEPER_HOME=\"${ZOOKEEPER_HOME}\"
-  export ACCUMULO_HOME=\"${ACCUMULO_HOME}\"
-  export PATH=\${QI_HOME}/bin:\${HADOOP_HOME}/bin:\${ZOOKEEPER_HOME}/bin:\${ACCUMULO_HOME}/bin:\${PATH}
-fi"
-  echo "${cloudenv}" > ${QI_HOME}/bin/cloud-env
-  chmod 755 ${QI_HOME}/bin/cloud-env
+  ENV_FILE="${QI_HOME}/bin/quickinstall-env"
+  echo "Setting up ${ENV_FILE}"
+  _replace_stuff "QI_QI_HOME" "${QI_HOME}" ${ENV_FILE}
+  _replace_stuff "QI_JAVA_HOME" "${JAVA_HOME}" ${ENV_FILE}
+  _replace_stuff "QI_HADOOP_HOME" "${HADOOP_HOME}" ${ENV_FILE}
+  _replace_stuff "QI_ZOOKEEPER_HOME" "${ZOOKEEPER_HOME}" ${ENV_FILE}
+  _replace_stuff "QI_ACCUMULO_HOME" "${ACCUMULO_HOME}" ${ENV_FILE}
+  chmod 755 ${ENV_FILE}
   # make it harder to run the setup again
   chmod 400 $(_script_dir)/setup-quickinstall.sh
   echo "Accumulo is now running from ${QI_HOME}"
   echo "You should run"
-  echo "    source ${QI_HOME}/bin/cloud-env"
+  echo "    source ${ENV_FILE}"
   echo "and get to work.  Here are some useful links."
   echo "  The Accumulo monitor page should be available at http://localhost:50095"
   echo "  HDFS should be available at http://localhost:50070"
